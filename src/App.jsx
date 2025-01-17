48d2134b838638248bb3948113b2948ca4947b19
Hariks17 <hariksh.94@gmail.com>
2025-01-17 13:20:30 +0530

    Added Assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div classname="w-full overflow-hidden">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
