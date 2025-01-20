ff839aa3daa9a8e141b9cb8e17fb8cd239e2596b
Hariks17 <hariksh.94@gmail.com>
2025-01-20 16:58:15 +0530

    Finished

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,38 @@
-import React from "react";
-import { assets } from "../assets/assets";
+import React, { useEffect, useState } from "react";
+import { assets, projectsData } from "../assets/assets";
+import { motion } from "motion/react";
 
 const Projects = () => {
+  const [index, setIndex] = useState(0);
+  const [noOfCards, setNoOfCards] = useState(1);
+
+  useEffect(() => {
+    const updateNoOfCards = () => {
+      if (window.innerWidth >= 1024) {
+        setNoOfCards(projectsData.length);
+      } else {
+        setNoOfCards(1);
+      }
+    };
+    updateNoOfCards();
+    window.addEventListener("resize", updateNoOfCards);
+    return () => window.removeEventListener("resize", updateNoOfCards);
+  }, []);
+
+  const slideNext = () => {
+    setIndex((prev) => (prev + 1) % projectsData.length);
+  };
+
+  const slidePrev = () => {
+    setIndex((prev) => (prev === 0 ? projectsData.length - 1 : prev - 1));
+  };
+
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, x: -200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
       className="container mx-auto py-4 pt-20 px-6
     md:px-20 lg:px-32 w-full overflow-hidden
     "
@@ -23,6 +52,7 @@ const Projects = () => {
         <button
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Previous Project"
+          onClick={slidePrev}
         >
           <img src={assets.left_arrow} alt="Pervious" />
         </button>
@@ -30,11 +60,40 @@ const Projects = () => {
         <button
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Next Project"
+          onClick={slideNext}
         >
           <img src={assets.right_arrow} alt="Next" />
         </button>
       </div>
-    </div>
+
+      <div className="overflow-hidden">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${(index * 100) / noOfCards}%)` }}
+        >
+          {projectsData.map((project, i) => (
+            <div key={i} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 shadow-md">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span className="px-1"> | </span>{" "}
+                    {project.location}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
